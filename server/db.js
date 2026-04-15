b1df41e288770ccf52525a2e4aad3ac84391da44
KarolinaBittman <karolina.bittman@gmail.com>
2026-04-15 07:01:31 -0400

    update

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<zmLCcqbQfM2qJ5prD2s8MRQy>@172.21.107.89:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
